0603f404ec64a05439ba89aa55fe8b376dfd2630
11bluetree <lax.bluetree@hotmail.co.jp>
2022-01-07 23:25:33 +0900

    s

---
+++ jpa/jpa-pra/src/main/java/com/example/jpapra/JpaPraApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpapra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPraApplication.class, args);
+	}
+
+}
---
+++ jpa/jpa-pra/src/test/java/com/example/jpapra/JpaPraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpapra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
