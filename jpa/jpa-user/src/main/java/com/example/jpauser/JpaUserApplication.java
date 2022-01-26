f77e0c58f9a71dfe906a708fac56d944a3762ed7
11bluetree <lax.bluetree@hotmail.co.jp>
2022-01-27 00:53:20 +0900

    初期作成

---
+++ jpa/jpa-user/src/main/java/com/example/jpauser/JpaUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUserApplication.class, args);
+	}
+
+}
